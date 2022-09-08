2666d233ea7ee3274ade93c5830012fce0d74ff0
ud17 <upandya1297@gmail.com>
2022-09-08 12:40:33 -0400

    List Component added

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import "../styles/List.css";
+import {AiFillEye} from "react-icons/ai";
 
 function List() {
   return (
-    <div>List</div>
+    <div className='list'>
+      <div className='card'>
+        <img src={"../images/display-picture.jpg"}/>
+        <h4>Niagara Falls</h4>
+        <div className='view'>
+          <AiFillEye/>
+          <h5>125</h5>
+        </div>        
+      </div>
+    </div>
   )
 }
 
