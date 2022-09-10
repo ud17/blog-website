d629d4bf56bb3da643b3e677df54a2cf156bce62
ud17 <upandya1297@gmail.com>
2022-09-10 12:12:38 -0400

    responsive design

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import "../styles/AboutMe.css";
 
 function AboutMe() {
   return (
-    <AboutWrapper>
-      <div>
-        <DisplayImage src={"../images/display-picture.jpg"} alt="display-picture"/>
-      </div>
-      <Info>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+    <div className='about-card'>
+      <img src={"../images/display-picture.jpg"} alt="display-picture"/>
+      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
           ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
-      </Info>
-    </AboutWrapper>
+    </div>
   )
 }
 
@@ -19,6 +16,7 @@ const AboutWrapper = styled.div`
   margin-top: 5rem;
   margin-bottom: 5rem;
   display: flex;
+  flex-wrap: wrap;
   justify-content: flex-start;
   padding: 1% 10%;
 
