cca8a2883c756ac054a36270502070556217b379
ud17 <upandya1297@gmail.com>
2022-09-12 07:59:16 -0400

    Footer Component finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import "../styles/Footer.css";
+const LINK = require("../utils/Links");
 
 function Footer() {
   return (
     <div className='footer'>
-      Footer
+      <a href={LINK.github} target="_blank"><img src={"../images/github.png"} /></a>
+      <a href={LINK.instagram} target="_blank"><img src={"../images/instagram.png"} /></a>
+      <a href={LINK.linkedin} target="_blank"><img src={"../images/linkedin.png"} /></a>
     </div>
   )
 }
---
+++ src/utils/Links.js
@@ -0,0 +1,6 @@
+module.exports = {
+    github : 'https://github.com/ud17',
+    instagram: 'https://www.instagram.com/uditpandya12/',
+    linkedin: 'https://www.linkedin.com/in/uditpandya12/' 
+};
+
