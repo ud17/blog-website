be5a1aa3ee44472e278b7de7d199ffa905815b43
ud17 <upandya1297@gmail.com>
2022-10-03 15:31:30 -0400

    about me page finished

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,9 +5,12 @@ import "../styles/AboutMe.css";
 function AboutMe() {
   return (
     <div className='about-card'>
-      <img src={"../images/display-picture.jpg"} alt="display-picture"/>
-      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+      <div className='card-img'>
+        <img src={"../images/display-picture.jpg"} alt="display-picture"/>
+      </div>      
+      <h1><i>"Travel is the only thing you buy that makes you richer"</i></h1>
+      <h3>Welcome to my travel blog, Love Travelling. I’m Udit, an independent traveller writing about my visits to countries near and far.
+          I research destinations myself without the need for a tour guide or packaged trip.  That’s part of the fun – visiting the local library and selecting some travel guides, looking things up online and, of course, all the amazing tips I discover from fellow travel bloggers.</h3>
     </div>
   )
 }
