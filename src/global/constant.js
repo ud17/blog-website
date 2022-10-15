2efd46e18f7758c64197d59cf75b7f41bdf0cc80
ud17 <upandya1297@gmail.com>
2022-10-15 13:07:42 -0400

    production url in place

--- src/global/constant.js
+++ src/global/constant.js
@@ -1,6 +1,6 @@
 const localhostBaseUrl = process.env.REACT_APP_LOCAL_URL;
 const prodBaseUrl = process.env.REACT_APP_PRODUCTION_URL;
-let isProduction = false;
+let isProduction = true;
 
 module.exports = {
     // base url local machine
