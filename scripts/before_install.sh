df250cf6f96f87a5b25a9b17c5215c98dc17ae05
ud17 <upandya1297@gmail.com>
2022-10-16 00:19:32 -0400

    added sudo permission

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/blog-frontend
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-yum -y install nodejs npm
+sudo curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum -y install nodejs npm
