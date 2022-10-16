01b17004f818a71ac8220dfcdd63d5b24bf438a0
ud17 <upandya1297@gmail.com>
2022-10-16 00:00:56 -0400

    app_start script modified

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/blog-frontend/src
-npm start
-pm2 start npm --name "blog-frontend" -- start
+# npm start
+pm2 stop 0
+pm2 start --name "blog-frontend" npm -- start
 pm2 startup
-pm2 save
+pm2 save --force
 pm2 restart all
